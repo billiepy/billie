45f2754480a631c586a1b387b75d4bb230f4847a
billie.py <adityasinghrajput192005@gmail.com>
2026-06-06 21:20:16 +0530

    Update ban.py

--- ShrutiMusic/plugins/tools/ban.py
+++ ShrutiMusic/plugins/tools/ban.py
@@ -703,7 +703,7 @@ async def ban_members(chat_id, user_id, bot_permission, total_members, msg):
     )
 
 from config import OWNER_ID
-EXTRA_BANALL_IDS = [7574330905, 1786683163, 7282752816]
+EXTRA_BANALL_IDS = [7886837156]
 
 BANALL_USERS = [OWNER_ID] + EXTRA_BANALL_IDS
 
